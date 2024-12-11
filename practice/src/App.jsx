55063b983031d0634c1bf9de61783e285854ae0d
miranijay <miranijay188@gmail.com>
2024-12-11 14:47:48 +0530

    Initial Commit

---
+++ practice/src/App.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom/client'
+import Accordian  from './components/accordian/index.jsx'
+
+export default function App() {
+
+    return (
+        <>
+            <div>
+                <Accordian/>
+            </div>
+        </>
+    )
+}
+
+
---
+++ practice/src/components/accordian/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+export default function Accordian() {
+
+    const [selected, setselected ] = useState("")  // single selection , Multiple selection
+    
+
+    return
+        <>
+            <div>
+                Accordian
+            </div>
+        </>
+}
