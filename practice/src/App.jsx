ff1762b207ed84bbdbe622f8762fcf04e4ef33fb
miranijay <miranijay188@gmail.com>
2024-12-11 18:17:52 +0530

    created RandomColor

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom/client'
 import Accordian  from './components/accordian/index.jsx'
+import Randomcolor from './components/random-color/index.jsx'
 
 export default function App() {
 
     return (
             <div className='App'>
-                <Accordian />
+
+                {/* <Accordian /> */}
+
+                <Randomcolor />
+
+
             </div>
     )
 }
---
+++ practice/src/components/random-color/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+export default function Randomcolor() {
+
+    const [typeColor, settypeColor] = useState("hex")
+    const [color, setcolo] = useState("#000000")
+
+    function handleRandomHexColor () {
+
+    }
+
+    function handleRandomrgbColor () {
+
+    }
+    
+
+    return (
+        <div className="container" style={{
+            width:"100vw",
+            height: "100vh",
+            background: color, 
+            }}>
+            <button onClick={() => settypeColor('hex')}>Generate Hex Color </button>
+            <button onClick={() => settypeColor('rgb')}>Generate RGB Color</button>
+            <button onClick={typeColor === 'hex' ? handleRandomHexColor : handleRandomrgbColor}>Generate Random Color</button>
+        </div>
+    )
+}
