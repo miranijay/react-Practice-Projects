146fffe619a5befa8f6dae10209cc9835346e4f4
miranijay <miranijay188@gmail.com>
2025-01-01 17:12:55 +0530

    image-slider

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import Accordian  from './components/accordian/index.jsx'
 import Randomcolor from './components/random-color/index.jsx'
 import StarRating from './components/star_rating/index.jsx'
+import ImageSlider from './components/image_slider/index.jsx'
 
 export default function App() {
 
@@ -12,7 +13,13 @@ export default function App() {
 
                 <Randomcolor />
 
-                <StarRating />
+                <StarRating stars={10}/>
+
+                <ImageSlider 
+                    url={"https://picsum.photos/v2/list"}
+                    page={"1"}
+                    limit={"10"}
+                />
 
 
             </div>
